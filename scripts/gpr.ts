b7ce92116d2b5ca7366ea5880b5f6750b8434a14
Snazzah <suggesttosnazzy@gmail.com>
2020-12-16 12:34:41 -0600

    Clean up docs branch

--- scripts/gpr.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import { join } from 'path';
-
-import pkg from '../package.json';
-
-pkg.name = `@snazzah/${pkg.name}`;
-
-fs.writeFileSync(join(__dirname, '../package.json'), JSON.stringify(pkg));

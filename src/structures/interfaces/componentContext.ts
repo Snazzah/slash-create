14bc69c3197fbfa90ccf5cf4688c334aaae60295
Snazzah <suggesttosnazzy@gmail.com>
2021-05-24 13:15:51 -0500

    feat(componentContext): Add missing props

--- src/structures/interfaces/componentContext.ts
+++ src/structures/interfaces/componentContext.ts
@@ -1,4 +1,4 @@
-import { MessageComponentRequestData } from '../../constants';
+import { ComponentType, MessageComponentRequestData, PartialMessage } from '../../constants';
 import MessageInteractionContext from './messageInteraction';
 import SlashCreator from '../../creator';
 import { RespondFunction } from '../../server';
@@ -7,7 +7,15 @@ import { RespondFunction } from '../../server';
 class ComponentContext extends MessageInteractionContext {
   /** The request data. */
   readonly data: MessageComponentRequestData;
-  /** The timeout for the auto-response. */
+
+  /** The ID of the component to identify its origin from. */
+  readonly customID: string;
+  /** The type of component this interaction came from. */
+  readonly componentType: ComponentType;
+  /** The partial message this interaction came from. */
+  readonly message: PartialMessage;
+
+  /** @hidden */
   private _timeout?: any;
 
   /**
@@ -18,11 +26,17 @@ class ComponentContext extends MessageInteractionContext {
   constructor(creator: SlashCreator, data: MessageComponentRequestData, respond: RespondFunction) {
     super(creator, data, respond);
     this.data = data;
+
+    this.customID = data.data.custom_id;
+    this.componentType = data.data.component_type;
+    this.message = data.message;
+
+    // Auto-acknowledge if no response was given in 2 seconds
+    this._timeout = setTimeout(() => this.acknowledge(), 2000);
   }
 
   /**
    * Acknowledges the interaction without replying.
-   * @param ephemeral Whether to make the deferred message ephemeral.
    * @returns Whether the acknowledgement passed passed
    */
   async acknowledge(): Promise<boolean> {

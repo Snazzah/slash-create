7ab7a59796054c9caa22967a1f126bdfa00382e3
ytausch <dev@ytausch.de>
2021-05-25 14:18:32 +0200

    Add unit tests for AWS Lambda support (exports check / lambdaHeaders)

--- test/index.ts
+++ test/index.ts
@@ -7,6 +7,7 @@ import * as slashCreate from '../src';
 describe('[index]', () => {
   it('exports check', () => {
     expect(slashCreate.API).to.be.a('function');
+    expect(slashCreate.AWSLambdaServer).to.be.a('function');
     expect(slashCreate.BitField).to.be.a('function');
     expect(slashCreate.Command).to.be.a('function');
     expect(slashCreate.CommandContext).to.be.a('function');

60c81bb96e4be9c071b18a74591e9ff1f97237be
Junior <sudojunior@outlook.com>
2021-10-02 22:00:23 +0100

    reflect properties and methods onto `Role` class * creator instance was required to get image defaults for format and size

--- src/structures/role.ts
+++ src/structures/role.ts
@@ -1,5 +1,6 @@
 /* global BigInt */
-import { ResolvedRole } from '../constants';
+import { CDN_URL, Endpoints, ImageFormat, ImageFormats, ImageSizeBoundaries, ResolvedRole } from '../constants';
+import { SlashCreator } from '../creator';
 import { Permissions } from './permissions';
 
 /** Represents a resolved role object. */
@@ -14,10 +15,17 @@ export class Role {
   readonly color: number;
   /** Whether the role is being hoisted */
   readonly hoist: boolean;
+  /** The role icon hash */
+  readonly icon?: string;
   /** Whether the role is being managed by an application */
   readonly managed: boolean;
   /** Whether the role is mentionable by everyone */
   readonly mentionable: boolean;
+  /** The role unicode emoji */
+  readonly unicodeEmoji?: string;
+
+  /** The creator of the role class. */
+  private _creator: SlashCreator;
 
   private _permissionsBitfield?: Permissions;
   private _permissions: string;
@@ -25,15 +33,42 @@ export class Role {
   /**
    * @param data The data for the member
    */
-  constructor(data: ResolvedRole) {
+  constructor(data: ResolvedRole, creator: SlashCreator) {
+    this._creator = creator;
+
     this.id = data.id;
     this.name = data.name;
     this.position = data.position;
     this.color = data.color;
     this.hoist = data.hoist;
+    if (data.icon) this.icon = data.icon;
     this.managed = data.managed;
     this.mentionable = data.mentionable;
+    if (data.unicode_emoji) this.unicodeEmoji = data.unicode_emoji;
     this._permissions = data.permissions;
+  }
+
+  /** The URL of the role icon. */
+  get iconURL() {
+    return this.dynamicIconURL();
+  }
+
+  /**
+   * Get the role's icon with the given format and size.
+   * @param format The format of the icon
+   * @param size The size of the icon
+   */
+  dynamicIconURL(format?: ImageFormat, size?: number) {
+    if (!this.icon) return null;
+    if (!format || !ImageFormats.includes(format.toLowerCase())) {
+      format = this._creator.options.defaultImageFormat;
+    }
+
+    if (!size || size < ImageSizeBoundaries.MINIMUM || size > ImageSizeBoundaries.MAXIMUM) {
+      size = this._creator.options.defaultImageSize;
+    }
+
+    return `${CDN_URL}${Endpoints.ROLE_ICON(this.id, this.icon)}.${format}?size=${size}`;
   }
 
   /** The string that mentions this role. */

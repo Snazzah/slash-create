0a895bef091c38883563af6e91e9a459128b7862
Snazzah <suggesttosnazzy@gmail.com>
2022-02-09 08:51:41 -0600

    chore(docs): update docs on sendModal

--- src/structures/interfaces/modalSendableContext.ts
+++ src/structures/interfaces/modalSendableContext.ts
@@ -9,33 +9,42 @@ export class ModalSendableContext extends MessageInteractionContext {
     super(creator, data, respond);
   }
 
-  sendModal(payload: ModalOptions, callback?: ModalRegisterCallback): string {
+  /**
+   * Sends a modal to the user.
+   * If a callback is not defined, you are required to provide a custom ID in the options.
+   * If a callback is defined, a custom ID will be generated if not defined.
+   * @param options The message options
+   * @param callback The callback of the modal
+   * @returns The custom ID of the modal
+   */
+  sendModal(options: ModalOptions, callback?: ModalRegisterCallback): string {
     if (this.expired) throw new Error('This interaction has expired');
     if (this.initiallyResponded) throw new Error('This interaction has already responded.');
 
     if (callback) {
-      if (!payload.custom_id) payload.custom_id = generateID();
+      if (!options.custom_id) options.custom_id = generateID();
 
-      const key = `${this.user.id}-${payload.custom_id}`;
+      const key = `${this.user.id}-${options.custom_id}`;
 
       this.creator._modalCallbacks.set(key, {
         callback,
         expires: this.invokedAt + 1000 * 60 * 15
       });
-    } else if (!callback && !payload.custom_id)
+    } else if (!callback && !options.custom_id)
       throw new Error('Modal must have a custom_id if no callback is provided');
 
+    this.initiallyResponded = true;
     // @ts-expect-error
     clearTimeout(this._timeout);
     this._respond({
       status: 200,
       body: {
         type: InteractionResponseType.MODAL,
-        data: payload
+        data: options
       }
     });
 
-    return payload.custom_id!;
+    return options.custom_id!;
   }
 }
 

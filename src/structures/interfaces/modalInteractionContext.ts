98e44b97d8159b11c8e712dcb0ff145fa5edde9b
Junior <sudojunior@outlook.com>
2022-03-10 20:26:45 +0000

    feat: Add `#editParent` and `#data` to `ModalInteractionContext` (#263)

--- src/structures/interfaces/modalInteractionContext.ts
+++ src/structures/interfaces/modalInteractionContext.ts
@@ -6,12 +6,18 @@ import {
 } from '../../constants';
 import { SlashCreator } from '../../creator';
 import { RespondFunction } from '../../server';
-import { MessageInteractionContext } from './messageInteraction';
+import { formatAllowedMentions, FormattedAllowedMentions } from '../../util';
+import { Message } from '../message';
+import { EditMessageOptions, MessageInteractionContext } from './messageInteraction';
 
 /** Represents an interaction context from a modal submission. */
 export class ModalInteractionContext extends MessageInteractionContext {
+  /** The request data. */
+  readonly data: ModalSubmitRequestData;
   /** The ID of the component to identify its origin from. */
   readonly customID: string;
+  /** The message this interaction came from, will be partial for ephemeral messages. */
+  readonly message?: Message;
 
   /** The values defined in the modal submission. */
   readonly values: { [key: string]: string };
@@ -25,9 +31,11 @@ export class ModalInteractionContext extends MessageInteractionContext {
   constructor(creator: SlashCreator, data: ModalSubmitRequestData, respond: RespondFunction, useTimeout = true) {
     super(creator, data, respond);
 
+    this.data = data;
     this.customID = data.data.custom_id;
     this.values = ModalInteractionContext.convertComponents(data.data.components);
 
+    if (data.message) this.message = new Message(data.message, creator, this);
     // Auto-defer if no response was given in 2 seconds
     if (useTimeout) this._timeout = setTimeout(() => this.defer(false), 2000);
   }
@@ -62,5 +70,47 @@ export class ModalInteractionContext extends MessageInteractionContext {
     }
 
     return false;
+  }
+
+  /*
+   * Edits the message that the component interaction came from.
+   * This will return a boolean if it's an initial response, otherwise a {@link Message} will be returned.
+   * @param content The content of the message
+   * @param options The message options
+   */
+  async editParent(content: string | EditMessageOptions, options?: EditMessageOptions): Promise<boolean | Message> {
+    if (this.expired) throw new Error('This interaction has expired');
+    if (!this.message) throw new Error('This interaction has no message');
+
+    if (typeof content !== 'string') options = content;
+    else if (typeof options !== 'object') options = {};
+
+    if (typeof options !== 'object') throw new Error('Message options is not an object.');
+
+    if (!options.content && typeof content === 'string') options.content = content;
+
+    if (!options.content && !options.embeds && !options.components) throw new Error('No valid options were given.');
+
+    const allowedMentions = options.allowedMentions
+      ? formatAllowedMentions(options.allowedMentions, this.creator.allowedMentions as FormattedAllowedMentions)
+      : this.creator.allowedMentions;
+
+    if (!this.initiallyResponded) {
+      this.initiallyResponded = true;
+      clearTimeout(this._timeout);
+      await this._respond({
+        status: 200,
+        body: {
+          type: InteractionResponseType.UPDATE_MESSAGE,
+          data: {
+            content: options.content,
+            embeds: options.embeds,
+            allowed_mentions: allowedMentions,
+            components: options.components
+          }
+        }
+      });
+      return true;
+    } else return this.edit(this.message.id, content, options);
   }
 }

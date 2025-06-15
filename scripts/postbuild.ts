a97e7f9a6abaa7f77fc31397d99243e757146c2a
Snazzah <me@snazzah.com>
2025-06-15 10:16:53 -0500

    fix(scripts): fix postbuild

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -5,5 +5,5 @@ const filePath = join(__dirname, '../lib/node/creator.js');
 
 fs.writeFileSync(
   filePath,
-  fs.readFileSync(filePath, { encoding: 'utf-8' }).replace('require(filePath)', 'await import(filePath)')
+  fs.readFileSync(filePath, { encoding: 'utf-8' }).replace('require(filePath)', '(await import(filePath)).default')
 );
